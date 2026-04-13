4e90bb796035769a6eadd3ce09ad5511df0b666a
MatteoCancelli <matteo.cancelli.5@gmail.com>
2026-04-13 17:27:14 +0200

    salvo lo stato di calibrazione su flash esp32

--- BME680.cpp
+++ BME680.cpp
@@ -2,8 +2,40 @@
 #include "include/BME680.h"
 #include "include/Logic.h"
 #include <bsec.h>
+#include <Preferences.h>
 
 Bsec bsec;
+Preferences prefs;
+
+#define BSEC_STATE_KEY "bsec_state"
+
+static void load_bsec_state()
+{
+  prefs.begin("bsec", true);
+  size_t len = prefs.getBytesLength(BSEC_STATE_KEY);
+  if (len == BSEC_MAX_STATE_BLOB_SIZE)
+  {
+    uint8_t state[BSEC_MAX_STATE_BLOB_SIZE];
+    prefs.getBytes(BSEC_STATE_KEY, state, len);
+    bsec.setState(state);
+    Serial.println("Stato BSEC caricato dalla flash");
+  }
+  else
+  {
+    Serial.println("Nessuno stato BSEC salvato, calibrazione da zero");
+  }
+  prefs.end();
+}
+
+static void save_bsec_state()
+{
+  uint8_t state[BSEC_MAX_STATE_BLOB_SIZE];
+  bsec.getState(state);
+  prefs.begin("bsec", false);
+  prefs.putBytes(BSEC_STATE_KEY, state, BSEC_MAX_STATE_BLOB_SIZE);
+  prefs.end();
+  Serial.println("Stato BSEC salvato in flash");
+}
 
 void check_bme()
 {
@@ -24,6 +56,8 @@ void check_bme()
 
   bsec.updateSubscription(sensor_list, 3, BSEC_SAMPLE_RATE_LP);
 
+  load_bsec_state();
+
   Serial.println("BME680 + BSEC OK!");
   bme_ok_msg();
   delay(1000);
@@ -31,6 +65,8 @@ void check_bme()
 
 void task_bme(void *pvParameters)
 {
+  uint32_t save_counter = 0;
+
   for (;;)
   {
     if (bsec.run())
@@ -38,6 +74,13 @@ void task_bme(void *pvParameters)
       temp      = bsec.temperature;
       hum       = bsec.humidity;
       gas_index = bsec.iaq;
+
+      save_counter++;
+      if (save_counter >= 300)
+      {
+        save_bsec_state();
+        save_counter = 0;
+      }
     }
     else if (bsec.bsecStatus != BSEC_OK || bsec.bme68xStatus != BME68X_OK)
     {
